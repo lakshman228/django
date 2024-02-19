b53d434864743f2d8bae2eafb30c532991e3abec
lakshman228 <82862017+lakshman228@users.noreply.github.com>
2024-02-19 16:20:34 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cd /var/www/django
 git pull
 
 # install the app dependencies
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # run the databse migration
 python manage.py migrate
