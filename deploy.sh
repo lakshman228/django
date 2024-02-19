57ab8448e9448faa0a0f3fc9e9b4f2a0c074f242
lakshman228 <82862017+lakshman228@users.noreply.github.com>
2024-02-19 17:08:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,14 @@ source ./.venv/bin/activate
 # Cd into the project code
 cd /var/www/polling
 
+# pull the latest codebase
+git pull
+
+# install the app dependencies
+pip install -r requirements.txt
+
+# run the databse migration
+python3 manage.py migrate
+
+
 
