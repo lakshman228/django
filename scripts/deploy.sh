c2ba223279ee026eabecae8af85004a5673eefd9
lakshman228 <82862017+lakshman228@users.noreply.github.com>
2024-02-19 16:19:35 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,16 +15,6 @@ pip3 install -r requirements.txt
 # run the databse migration
 python manage.py migrate
 
-# run the collect static command
-#python manage.py collectstatic 
-
-# put all other commads that required for you specific app
 
 # deactivate
 deactivate
-
-# reload nginx
-sudo systemctl reload nginx
-
-# restart the gunicorn
-sudo systemctl restart gunicorn
